21243d1ad645041440b758dbaaa8e0941481552a
Zendrique <40753927+zendrique@users.noreply.github.com>
2021-02-09 21:46:04 +0100

    update and translate

--- app/assets/js/scripts/landing.js
+++ app/assets/js/scripts/landing.js
@@ -227,7 +227,7 @@ const refreshServerStatus = async function(fade = false){
         const serverURL = new URL('my://' + serv.getAddress())
         const servStat = await ServerStatus.getStatus(serverURL.hostname, serverURL.port)
         if(servStat.online){
-            pLabel = 'Joeur'
+            pLabel = 'Joueur'
             pVal = servStat.onlinePlayers + '/' + servStat.maxPlayers
         }
 
--- app/assets/js/scripts/uicore.js
+++ app/assets/js/scripts/uicore.js
@@ -55,8 +55,8 @@ if(!isDev){
                 populateSettingsUpdateInformation(info)
                 break
             case 'update-downloaded':
-                loggerAutoUpdaterSuccess.log('Update ' + info.version + ' ready to be installed.')
-                settingsUpdateButtonStatus('Install Now', false, () => {
+                loggerAutoUpdaterSuccess.log('Mise à jour ' + info.version + ' prêt à être installé.')
+                settingsUpdateButtonStatus('Installer maintenant', false, () => {
                     if(!isDev){
                         ipcRenderer.send('autoUpdateAction', 'installUpdateNow')
                     }
@@ -65,7 +65,7 @@ if(!isDev){
                 break
             case 'update-not-available':
                 loggerAutoUpdater.log('No new update found.')
-                settingsUpdateButtonStatus('Check for Updates')
+                settingsUpdateButtonStatus('Vérifier les mises à jour')
                 break
             case 'ready':
                 updateCheckListener = setInterval(() => {
