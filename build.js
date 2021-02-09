17225457b17e189d949ca4ea448f385803277001
Zendrique <40753927+zendrique@users.noreply.github.com>
2021-02-09 19:29:34 +0100

    First update

--- build.js
+++ build.js
@@ -18,10 +18,10 @@ function getCurrentPlatform(){
 builder.build({
     targets: (process.argv[2] != null && Platform[process.argv[2]] != null ? Platform[process.argv[2]] : getCurrentPlatform()).createTarget(),
     config: {
-        appId: 'helioslauncher',
-        productName: 'Helios Launcher',
+        appId: 'turfulauncher',
+        productName: 'Turfu',
         artifactName: '${productName}-setup-${version}.${ext}',
-        copyright: 'Copyright © 2018-2020 Daniel Scalzi',
+        copyright: 'Copyright © 2018-2021 Daniel Scalzi, Zendrique',
         directories: {
             buildResources: 'build',
             output: 'dist'
@@ -46,10 +46,10 @@ builder.build({
         },
         linux: {
             target: 'AppImage',
-            maintainer: 'Daniel Scalzi',
-            vendor: 'Daniel Scalzi',
-            synopsis: 'Modded Minecraft Launcher',
-            description: 'Custom launcher which allows users to join modded servers. All mods, configurations, and updates are handled automatically.',
+            maintainer: 'Daniel Scalzi, Zendrique',
+            vendor: 'Daniel Scalzi, Zendrique',
+            synopsis: 'Launcher de Turfu',
+            description: 'Turfu serveur Minecraft.',
             category: 'Game'
         },
         compression: 'maximum',
