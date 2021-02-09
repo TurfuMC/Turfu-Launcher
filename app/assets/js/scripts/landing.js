6ac9dc11310de9fa4a4d2fd68f0c2fbe63df14b8
Zendrique <40753927+zendrique@users.noreply.github.com>
2021-02-09 20:58:09 +0100

    Translate

--- app/assets/js/scripts/landing.js
+++ app/assets/js/scripts/landing.js
@@ -227,7 +227,7 @@ const refreshServerStatus = async function(fade = false){
         const serverURL = new URL('my://' + serv.getAddress())
         const servStat = await ServerStatus.getStatus(serverURL.hostname, serverURL.port)
         if(servStat.online){
-            pLabel = 'PLAYERS'
+            pLabel = 'Joeur'
             pVal = servStat.onlinePlayers + '/' + servStat.maxPlayers
         }
 
